b8b310534e53c77914c0bf7fe5667097ce0f9c1d
ykamoji <ykamoji@umass.edu>
2025-04-08 15:12:13 -0400

    Adding utils

---
+++ code/utils.py
@@ -0,0 +1,67 @@
+import numpy as np
+import os
+import errno
+import matplotlib.pyplot as plt 
+from skimage.feature import plot_matches
+
+# This code is part of:
+#
+#   CMPSCI 670: Computer Vision, Spring 2024
+#   University of Massachusetts, Amherst
+#   Instructor: Grant Van Horn
+#
+
+def showMatches(im1, im2, c1, c2, matches, title="", output_path=None):
+    disp_matches = np.array([np.arange(matches.shape[0]), matches]).T.astype(int)
+    valid_matches = np.where(matches>=0)[0]
+    disp_matches = disp_matches[valid_matches, :]
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    #from IPython import embed; embed(); exit(-1)
+    plot_matches(ax, im1, im2, c1[:, [1,0]].astype(int), c2[:,[1,0]].astype(int), disp_matches,
+                 matches_color='dodgerblue', keypoints_color='k', only_matches=True)
+    ax.set_title(title)
+    plt.axis('off')
+    if output_path:
+        plt.savefig(output_path, bbox_inches='tight', edgecolor='auto')
+    # plt.show()
+
+
+def imread(path):
+    img = plt.imread(path).astype(float)
+    
+    #Remove alpha channel if it exists
+    if img.ndim > 2 and img.shape[2] == 4:
+        img = img[:, :, 0:3]
+    #Puts images values in range [0,1]
+    if img.max() > 1.0:
+        img /= 255.0
+
+    return img
+
+
+def mkdir(dirpath):
+    if not os.path.exists(dirpath):
+        try:
+            os.makedirs(dirpath)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    else:
+        print("Directory {} already exists.".format(dirpath))
+
+    
+#Thanks to ali_m from https://stackoverflow.com/questions/17190649
+def gaussian(hsize=3,sigma=0.5):
+    """
+    2D gaussian mask - should give the same result as MATLAB's
+    fspecial('gaussian',[shape],[sigma])
+    """
+    shape = (hsize, hsize)
+    m,n = [(ss-1.)/2. for ss in shape]
+    y,x = np.ogrid[-m:m+1,-n:n+1]
+    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
